26b61c69375bb994b98526371edc79f667dbc2fe
p34r1 <undeadgamer279@gmail.com>
2024-10-15 17:13:53 -0400

    horribly simplify

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -1,10 +1,3 @@
-# wrapper function for tmux
-function _fish_tmux_plugin_run
-    set tmux_cmd tmux
-
-    $tmux_cmd new-session
-end
-
 # this will autostart tmux if $fish_tmux_autostart is set to `true` using the `--on-variable` function option
 function _fish_tmux_plugin_run_autostart --on-variable fish_tmux_autostart
     if test "$fish_tmux_autostart" = true && \
@@ -14,6 +7,9 @@ function _fish_tmux_plugin_run_autostart --on-variable fish_tmux_autostart
     test -z "$NVIM" && \
     test -z "$VSCODE_RESOLVING_ENVIRONMENT" && \
     test "$TERM_PROGRAM" != 'vscode'
-        _fish_tmux_plugin_run
+
+        # tmux a       default   silence  if fails    new sesh    default    kill old fish (auto close when tmux closes
+        tmux attach -t default 2>/dev/null || tmux new-session -s default && kill $fish_pid
     end
 end
+
